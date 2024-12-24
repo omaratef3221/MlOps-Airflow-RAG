8ee5b2829c56a81051665c33f52bd62a2f10a3cf
omaratef3221 <omaratef3221@gmail.com>
2024-12-24 17:55:11 +0400

    professional keys

--- scripts/process_csv.py
+++ scripts/process_csv.py
@@ -1,8 +1,15 @@
 import boto3
 import os
+import os
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 
 def download_csv_from_s3():
-    s3 = boto3.client('s3',)
+    s3 = boto3.client('s3',
+                      aws_access_key_id = AWS_ACCESS_KEY,
+                      aws_secret_access_key = AWS_SECRET_KEY,
+                      region_name = 'us-east-1')
+    
     bucket_name = 'testorganizationbucket'
     object_key = 'netflix_titles.csv'
     local_file_path = './data/raw_movies.csv'
@@ -14,4 +21,4 @@ def download_csv_from_s3():
     print(f"Downloaded {object_key} to {local_file_path}", flush = True)
     return "Success"
     
-# download_csv_from_s3()
+download_csv_from_s3()
